929ab910e85024edc5daa3e595431971f982003c
Paul Savage <p.a.savage@btinternet.com>
2015-09-14 14:48:52 +0100

    Checkpoint 19 completed, storyboarding alcolator

---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Paul Savage on 14/09/2015.
+//  Copyright (c) 2015 Paul Savage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
