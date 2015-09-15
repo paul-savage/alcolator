85d4c0ff85943ca88df44987a47163da09a41ce1
Paul Savage <p.a.savage@btinternet.com>
2015-09-15 09:56:46 +0100

    Checkpoint 21 completed, basic navigation

---
+++ Alcolator/Alcolator/MainMenuViewController.m
@@ -0,0 +1,17 @@
+//
+//  MainMenuViewController.m
+//  Alcolator
+//
+//  Created by Paul Savage on 15/09/2015.
+//  Copyright (c) 2015 Paul Savage. All rights reserved.
+//
+
+#import "MainMenuViewController.h"
+
+@interface MainMenuViewController ()
+
+@end
+
+@implementation MainMenuViewController
+
+@end
